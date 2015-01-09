0d48d61a2dc914ba4d07035320aa7d8b26555970
Lewis Ellis <me@lewisjellis.com>
2015-01-08 18:23:21 -0800

    Specifies timeout is in milliseconds

--- examples/timeout.js
+++ examples/timeout.js
@@ -8,8 +8,8 @@ if (!stats.isSocket()) {
   throw new Error("Are you sure the docker is running?");
 }
 
-//you may specify a timeout for all operations, allowing to make sure you don't fall into limbo if something happens in docker
-var docker = new Docker({host: 'http://127.0.0.1', port: 2375, timeout: 1});
+// you may specify a timeout (in ms) for all operations, allowing to make sure you don't fall into limbo if something happens in docker
+var docker = new Docker({host: 'http://127.0.0.1', port: 2375, timeout: 100});
 
 docker.createContainer({Image: 'ubuntu', Cmd: ['/bin/bash']}, function (err, container) {
   container.start(function (err, data) {
