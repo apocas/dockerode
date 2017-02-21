7d0f18cf804f384c527fa46b083dab8435cf2b2c
Pedro Dias <petermdias@gmail.com>
2017-02-21 00:52:23 +0000

    v1.36 compliant

--- test/spec_helper.js
+++ test/spec_helper.js
@@ -1,24 +1,36 @@
 var http = require("http");
 http.globalAgent.maxSockets = 1000;
 
-var Docker = require('../lib/docker');
-var fs     = require('fs');
+var Docker = require('../lib/docker'),
+ fs = require('fs'),
+ isWin = require('os').type() === 'Windows_NT';
 
 // For Mac OS X:
 // socat -d -d unix-l:/tmp/docker.sock,fork tcp:<docker-host>:4243
 // DOCKER_SOCKET=/tmp/docker.sock npm test
 
-var socket   = process.env.DOCKER_SOCKET || '/var/run/docker.sock';
+// For Windows:
+// https://github.com/apocas/dockerode/issues/290#issuecomment-276393388
+// socketPath: '//./pipe/docker_engine'
+
+var socket = process.env.DOCKER_SOCKET || isWin ? '//./pipe/docker_engine' : '/var/run/docker.sock';
 var isSocket = fs.existsSync(socket) ? fs.statSync(socket).isSocket() : false;
 var docker;
 
 if (!isSocket) {
   console.log('Trying TCP connection...');
   docker = new Docker();
-  dockert = new Docker({timeout: 1});
+  dockert = new Docker({
+    timeout: 1
+  });
 } else {
-  docker = new Docker({ socketPath: socket });
-  dockert = new Docker({ socketPath: socket, timeout: 1 });
+  docker = new Docker({
+    socketPath: socket
+  });
+  dockert = new Docker({
+    socketPath: socket,
+    timeout: 1
+  });
 }
 
 module.exports = {
