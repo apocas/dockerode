077250d8f05d10061dd2b3bc3fa48e5fd1f731e8
ThePedro <73797168+ThePedroo@users.noreply.github.com>
2021-09-08 00:45:53 -0300

    Update top.js

--- examples/top.js
+++ examples/top.js
@@ -4,9 +4,7 @@ var fs     = require('fs');
 var socket = process.env.DOCKER_SOCKET || '/var/run/docker.sock';
 var stats  = fs.statSync(socket);
 
-if (!stats.isSocket()) {
-  throw new Error('Are you sure the docker is running?');
-}
+if (!stats.isSocket()) throw new Error('Are you sure the docker is running?');
 
 var docker = new Docker({host: 'http://127.0.0.1', port: 2375});
 
